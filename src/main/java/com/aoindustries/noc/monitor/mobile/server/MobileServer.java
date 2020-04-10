e799f3483730965950f70635c276846b6e54737a
AO Industries, Inc <support@aoindustries.com>
2020-04-10 15:49:16 -0500

    Converted top comment to LGPLv3 style.

--- src/main/java/com/aoindustries/noc/monitor/mobile/server/MobileServer.java
+++ src/main/java/com/aoindustries/noc/monitor/mobile/server/MobileServer.java
@@ -1,7 +1,24 @@
 /*
- * Copyright 2008-2012, 2020 by AO Industries, Inc.,
- * 7262 Bull Pen Cir, Mobile, Alabama, 36695, U.S.A.
- * All rights reserved.
+ * noc-monitor-mobile-server - Server for mobile access to Monitoring.
+ * Copyright (C) 2008-2012, 2020  AO Industries, Inc.
+ *     support@aoindustries.com
+ *     7262 Bull Pen Cir
+ *     Mobile, AL 36695
+ *
+ * This file is part of noc-monitor-mobile-server.
+ *
+ * noc-monitor-mobile-server is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * noc-monitor-mobile-server is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with noc-monitor-mobile-server.  If not, see <http://www.gnu.org/licenses/>.
  */
 package com.aoindustries.noc.monitor.mobile.server;
 
