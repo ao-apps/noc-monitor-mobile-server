b51e95da6f0f3f92b1dca60acf55041730aa33ad
AO Industries, Inc <support@aoindustries.com>
2022-04-16 02:03:24 -0500

    Added blank line before package declarations
    Resolves "EmptyLineSeparator: 'package' should be separated from previous line."

--- src/main/java/com/aoindustries/noc/monitor/mobile/server/MobileServer.java
+++ src/main/java/com/aoindustries/noc/monitor/mobile/server/MobileServer.java
@@ -1,6 +1,6 @@
 /*
  * noc-monitor-mobile-server - Server for mobile access to Monitoring.
- * Copyright (C) 2008-2012, 2020, 2021  AO Industries, Inc.
+ * Copyright (C) 2008-2012, 2020, 2021, 2022  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -20,6 +20,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with noc-monitor-mobile-server.  If not, see <https://www.gnu.org/licenses/>.
  */
+
 package com.aoindustries.noc.monitor.mobile.server;
 
 import com.aoindustries.noc.monitor.common.AlertLevel;
