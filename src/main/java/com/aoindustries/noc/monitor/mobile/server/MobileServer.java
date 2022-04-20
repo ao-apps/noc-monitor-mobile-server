1e31b73587e8cc88e891961cd00dcbc48e28dd42
AO Industries, Inc <support@aoindustries.com>
2022-04-20 05:03:40 -0500

    Significant manual code formatting clean-up…
    … in preparation for an automatic conversion to Google Java Style Guide:
    
    1) Added braces around if/else/for/while where missing
    
    2) Indentation is now 2-space, all file formats
    
      a) HTML indentation changed from tab to 2-space
    
      b) XML indentation changed from 4-space to 2-space
    
    3) Continuation is 4-space for JSON, Java, and JavaScript
    
    4) Updated NetBeans, kwrite, and kate settings

--- src/main/java/com/aoindustries/noc/monitor/mobile/server/MobileServer.java
+++ src/main/java/com/aoindustries/noc/monitor/mobile/server/MobileServer.java
@@ -51,162 +51,170 @@ import javax.net.ssl.SSLServerSocketFactory;
  */
 public class MobileServer {
 
-	private static final Logger logger = Logger.getLogger(MobileServer.class.getName());
+  private static final Logger logger = Logger.getLogger(MobileServer.class.getName());
 
-	private static final int PORT = 4585;
+  private static final int PORT = 4585;
 
-	/*private static int getNodeCount(NodeSnapshot snapshot) {
-		int total = 1;
-		for(NodeSnapshot child : snapshot.getChildren()) total += getNodeCount(child);
-		return total;
-	}*/
+  /*private static int getNodeCount(NodeSnapshot snapshot) {
+    int total = 1;
+    for (NodeSnapshot child : snapshot.getChildren()) total += getNodeCount(child);
+    return total;
+  }*/
 
-	private static void writeNodeTree(DataOutputStream out, NodeSnapshot node) throws IOException {
-		out.writeUTF(node.getLabel());
-		AlertLevel alertLevel = node.getAlertLevel();
-		switch(alertLevel) {
-			case NONE      : out.writeByte(0); break;
-			case LOW       : out.writeByte(1); break;
-			case MEDIUM    : out.writeByte(2); break;
-			case HIGH      : out.writeByte(3); break;
-			case CRITICAL  : out.writeByte(4); break;
-			case UNKNOWN   : out.writeByte(5); break;
-			default        : throw new AssertionError("Unexpected value for alertLevel: "+alertLevel);
-		}
-		String alertMessage = node.getAlertMessage();
-		if(alertMessage!=null && alertMessage.length()==0) alertMessage = null;
-		if(alertMessage!=null) {
-			out.writeBoolean(true);
-			out.writeUTF(alertMessage);
-		} else {
-			out.writeBoolean(false);
-		}
-		out.writeBoolean(node.getAllowsChildren());
-		List<NodeSnapshot> children = node.getChildren();
-		int numChildren = children.size();
-		if(numChildren>Short.MAX_VALUE) throw new IOException("Too many children for current protocol: "+numChildren);
-		out.writeShort(numChildren);
-		for(int c=0;c<numChildren;c++) writeNodeTree(out, children.get(c));
-	}
+  private static void writeNodeTree(DataOutputStream out, NodeSnapshot node) throws IOException {
+    out.writeUTF(node.getLabel());
+    AlertLevel alertLevel = node.getAlertLevel();
+    switch (alertLevel) {
+      case NONE      : out.writeByte(0); break;
+      case LOW       : out.writeByte(1); break;
+      case MEDIUM    : out.writeByte(2); break;
+      case HIGH      : out.writeByte(3); break;
+      case CRITICAL  : out.writeByte(4); break;
+      case UNKNOWN   : out.writeByte(5); break;
+      default        : throw new AssertionError("Unexpected value for alertLevel: "+alertLevel);
+    }
+    String alertMessage = node.getAlertMessage();
+    if (alertMessage != null && alertMessage.length() == 0) {
+      alertMessage = null;
+    }
+    if (alertMessage != null) {
+      out.writeBoolean(true);
+      out.writeUTF(alertMessage);
+    } else {
+      out.writeBoolean(false);
+    }
+    out.writeBoolean(node.getAllowsChildren());
+    List<NodeSnapshot> children = node.getChildren();
+    int numChildren = children.size();
+    if (numChildren>Short.MAX_VALUE) {
+      throw new IOException("Too many children for current protocol: "+numChildren);
+    }
+    out.writeShort(numChildren);
+    for (int c=0;c<numChildren;c++) writeNodeTree(out, children.get(c));
+  }
 
-	private final Monitor monitor;
-	private final String localAddress;
+  private final Monitor monitor;
+  private final String localAddress;
 
-	private final Object threadLock = new Object();
-	private Thread thread;
+  private final Object threadLock = new Object();
+  private Thread thread;
 
-	public MobileServer(Monitor monitor, String localAddress) {
-		this.monitor = monitor;
-		this.localAddress = localAddress;
-	}
+  public MobileServer(Monitor monitor, String localAddress) {
+    this.monitor = monitor;
+    this.localAddress = localAddress;
+  }
 
-	public void start() {
-		synchronized(threadLock) {
-			if(thread==null) {
-				thread = new Thread(
-					new Runnable() {
-						@Override
-						public void run() {
-							final ExecutorService executorService = ExecutorService.newInstance();
-							try {
-								final Thread currentThread = Thread.currentThread();
-								while(!currentThread.isInterrupted()) {
-									synchronized(threadLock) {
-										if(currentThread!=thread) break;
-									}
-									try {
-										ServerSocketFactory factory = SSLServerSocketFactory.getDefault();
-										ServerSocket ss;
-										if(localAddress==null) {
-											ss = factory.createServerSocket(PORT, 50);
-										} else {
-											InetAddress address = InetAddress.getByName(localAddress);
-											ss = factory.createServerSocket(PORT, 50, address);
-										}
-										try {
-											while(!currentThread.isInterrupted()) {
-												synchronized(threadLock) {
-													if(currentThread!=thread) break;
-												}
-												final Socket socket = ss.accept();
-												executorService.submitUnbounded(
-													new Runnable() {
-														@Override
-														public void run() {
-															try {
-																try {
-																	DataInputStream in = new DataInputStream(socket.getInputStream());
-																	try {
-																		// Check authentication
-																		String username = in.readUTF();
-																		String password = in.readUTF();
-																		RootNode rootNode; // Will be null if not authenticated
-																		try {
-																			rootNode = monitor.login(Locale.getDefault(), username, password);
-																		} catch(IOException err) {
-																			logger.log(Level.SEVERE, null, err);
-																			rootNode = null;
-																		}
-																		DataOutputStream out = new DataOutputStream(new GZIPOutputStream(socket.getOutputStream()));
-																		try {
-																			if(rootNode==null) {
-																				// Authentication failed
-																				out.writeBoolean(false);
-																			} else {
-																				// Authentication successful
-																				out.writeBoolean(true);
-																				// Write snapshot
-																				NodeSnapshot snapshot = rootNode.getSnapshot();
-																				//logger.log(Level.INFO, "RootNode snapshot has a total of "+getNodeCount(snapshot)+" nodes");
-																				writeNodeTree(out, snapshot);
-																			}
-																		} finally {
-																			out.close();
-																		}
-																	} finally {
-																		in.close();
-																	}
-																} finally {
-																	socket.close();
-																}
-															} catch(Exception err) {
-																logger.log(Level.SEVERE, null, err);
-															}
-														}
-													}
-												);
-											}
-										} finally {
-											ss.close();
-										}
-									} catch(ThreadDeath td) {
-										throw td;
-									} catch(Throwable t) {
-										logger.log(Level.SEVERE, null, t);
-										try {
-											Thread.sleep(60000);
-										} catch(InterruptedException err) {
-											// Normal on stop
-										}
-									}
-								}
-							} finally {
-								executorService.dispose();
-							}
-						}
-					}
-				);
-				thread.start();
-			}
-		}
-	}
+  public void start() {
+    synchronized (threadLock) {
+      if (thread == null) {
+        thread = new Thread(
+          new Runnable() {
+            @Override
+            public void run() {
+              final ExecutorService executorService = ExecutorService.newInstance();
+              try {
+                final Thread currentThread = Thread.currentThread();
+                while (!currentThread.isInterrupted()) {
+                  synchronized (threadLock) {
+                    if (currentThread != thread) {
+                      break;
+                    }
+                  }
+                  try {
+                    ServerSocketFactory factory = SSLServerSocketFactory.getDefault();
+                    ServerSocket ss;
+                    if (localAddress == null) {
+                      ss = factory.createServerSocket(PORT, 50);
+                    } else {
+                      InetAddress address = InetAddress.getByName(localAddress);
+                      ss = factory.createServerSocket(PORT, 50, address);
+                    }
+                    try {
+                      while (!currentThread.isInterrupted()) {
+                        synchronized (threadLock) {
+                          if (currentThread != thread) {
+                            break;
+                          }
+                        }
+                        final Socket socket = ss.accept();
+                        executorService.submitUnbounded(
+                          new Runnable() {
+                            @Override
+                            public void run() {
+                              try {
+                                try {
+                                  DataInputStream in = new DataInputStream(socket.getInputStream());
+                                  try {
+                                    // Check authentication
+                                    String username = in.readUTF();
+                                    String password = in.readUTF();
+                                    RootNode rootNode; // Will be null if not authenticated
+                                    try {
+                                      rootNode = monitor.login(Locale.getDefault(), username, password);
+                                    } catch (IOException err) {
+                                      logger.log(Level.SEVERE, null, err);
+                                      rootNode = null;
+                                    }
+                                    DataOutputStream out = new DataOutputStream(new GZIPOutputStream(socket.getOutputStream()));
+                                    try {
+                                      if (rootNode == null) {
+                                        // Authentication failed
+                                        out.writeBoolean(false);
+                                      } else {
+                                        // Authentication successful
+                                        out.writeBoolean(true);
+                                        // Write snapshot
+                                        NodeSnapshot snapshot = rootNode.getSnapshot();
+                                        //logger.log(Level.INFO, "RootNode snapshot has a total of "+getNodeCount(snapshot)+" nodes");
+                                        writeNodeTree(out, snapshot);
+                                      }
+                                    } finally {
+                                      out.close();
+                                    }
+                                  } finally {
+                                    in.close();
+                                  }
+                                } finally {
+                                  socket.close();
+                                }
+                              } catch (Exception err) {
+                                logger.log(Level.SEVERE, null, err);
+                              }
+                            }
+                          }
+                        );
+                      }
+                    } finally {
+                      ss.close();
+                    }
+                  } catch (ThreadDeath td) {
+                    throw td;
+                  } catch (Throwable t) {
+                    logger.log(Level.SEVERE, null, t);
+                    try {
+                      Thread.sleep(60000);
+                    } catch (InterruptedException err) {
+                      // Normal on stop
+                    }
+                  }
+                }
+              } finally {
+                executorService.dispose();
+              }
+            }
+          }
+        );
+        thread.start();
+      }
+    }
+  }
 
-	public void stop() {
-		synchronized(threadLock) {
-			if(thread != null) {
-				thread.interrupt();
-				thread = null;
-			}
-		}
-	}
+  public void stop() {
+    synchronized (threadLock) {
+      if (thread != null) {
+        thread.interrupt();
+        thread = null;
+      }
+    }
+  }
 }
